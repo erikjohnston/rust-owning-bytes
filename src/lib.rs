aca37142844694944eadfccaeae331111324af0d
Erik Johnston <erikj@jki.re>
2016-09-07 23:45:44 +0100

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,61 @@
 #![feature(unique, alloc, heap_api, question_mark)]
+#![warn(missing_docs)]
+
+/*!
+# Owning bytes
+
+This crate allows you to wrap up and parse around structs that have members that reference a block
+of bytes. This is especially useful for writing protocol parses where you want to do zero copy
+parsing.
+
+For example:
+
+```
+use owning_bytes::OwningByteBuf;
+
+struct ExampleParsed<'a> {
+    payload: &'a [u8],
+}
+
+impl<'a> ExampleParsed<'a> {
+    pub fn parse_buf(buf: &'a [u8]) -> ExampleParsed<'a> {
+        ExampleParsed { payload: &buf[1..3] }
+    }
+}
+
+fn create_from_vec(vec: Vec<u8>) -> OwningByteBuf<ExampleParsed<'static>> {
+    OwningByteBuf::from_vec(vec, ExampleParsed::parse_buf)
+}
+
+fn main() {
+    let vec = vec![1, 2, 3, 4];
+
+    let parsed = create_from_vec(vec);
+
+    assert_eq!(&parsed.get().payload, &[2, 3]);
+}
+```
+
+There are also methods for dealing with constructors that may fail:
+
+```
+use owning_bytes::OwningByteBuf;
+use std::str::{self, Utf8Error};
+
+
+fn create_from_vec(vec: Vec<u8>) -> Result<OwningByteBuf<&'static str>, Utf8Error> {
+    OwningByteBuf::from_vec_res(vec, str::from_utf8).map_err(|(err, _vec)| err)
+}
+
+fn main() {
+    let vec = b"Hello".to_vec();
+
+    let parsed = create_from_vec(vec).unwrap();
+
+    assert_eq!(*parsed.get(), "Hello");
+}
+```
+*/
 
 extern crate alloc;
 
@@ -9,7 +66,7 @@ use alloc::heap;
 
 use std::convert::AsRef;
 
-
+/// A wrapper around an array of bytes and an object T that references those bytes.
 pub struct OwningByteBuf<T> {
     resource: Unique<u8>,
     len: usize,
@@ -18,6 +75,7 @@ pub struct OwningByteBuf<T> {
 }
 
 impl<T> OwningByteBuf<T> {
+    /// Creates an OwningByteBuf from a vector and a constructing function
     pub fn from_vec<'a, F>(mut buf: Vec<u8>, f: F) -> OwningByteBuf<T> where F: FnOnce(&'a[u8]) -> T {
         let res = unsafe {
             let ptr = buf.as_mut_ptr();
@@ -34,6 +92,16 @@ impl<T> OwningByteBuf<T> {
         res
     }
 
+    /// Creates an OwningByteBuf from a vector and a constructing function that may fail
+    ///
+    /// ```
+    /// use std::str;
+    /// use owning_bytes::OwningByteBuf;
+    ///
+    /// let vec = b"Hello World".to_vec();
+    /// let string = OwningByteBuf::from_vec_res(vec, str::from_utf8).unwrap();
+    /// assert_eq!(*string.get(), "Hello World");
+    /// ```
     pub fn from_vec_res<'a, F, E>(mut buf: Vec<u8>, f: F) -> Result<OwningByteBuf<T>, (E, Vec<u8>)>
         where F: FnOnce(&'a[u8]) -> Result<T, E>
     {
@@ -55,6 +123,7 @@ impl<T> OwningByteBuf<T> {
         Ok(res)
     }
 
+    /// Creates an OwningByteBuf from a boxed slice and a constructing function
     pub fn from_box<'a, F>(mut buf: Box<[u8]>, f: F) -> OwningByteBuf<T> where F: FnOnce(&'a[u8]) -> T {
         let res = unsafe {
             let ptr = buf.as_mut_ptr();
@@ -70,6 +139,7 @@ impl<T> OwningByteBuf<T> {
         res
     }
 
+    /// Creates an OwningByteBuf from a boxed slice and a constructing function that may fail
     pub fn from_box_res<'a, F, E>(mut buf: Box<[u8]>, f: F) -> Result<OwningByteBuf<T>, (E, Box<[u8]>)>
         where F: FnOnce(&'a[u8]) -> Result<T, E>
     {
@@ -90,10 +160,12 @@ impl<T> OwningByteBuf<T> {
         Ok(res)
     }
 
+    /// Returns a reference to the wrapped type.
     pub fn get(&self) -> &T {
         &self.inner
     }
 
+    /// Drops the wrapped type and returns the underlying buffer back as a Vec.
     pub fn into_vec(mut self) -> Vec<u8> {
         let vec = {
             let OwningByteBuf { ref resource, len, cap, .. } = self;
